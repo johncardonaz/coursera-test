442d630b1c4c58280783cee7dfcb9cb74b04904a
johncardonaz <johncardonaz@coursera-test-com>
2023-04-05 15:03:50 -0500

    quiz

--- app.js
+++ app.js
@@ -14,13 +14,28 @@ function DIController ($scope,
     $scope.name = upCase($scope.name);
   };
 
-  console.log($injector.annotate(DIController));
 }
 
 function AnnonateMe(name, job, blah) {
   return "Blah!";
 }
 
-console.log(DIController.toString());
+var x1 = function () {
+  // do something, PLEASE!
+  return "Blah!";
+};
+
+var x2 = x1();
+
+function x3(arg) {
+  return arg; // Ha-ha! That's all I do!
+}
+
+var x4 = x3(x1);
+
+var x5 = x3(x2);
+
+var x6 = x3(x1());
+
 
 })();
